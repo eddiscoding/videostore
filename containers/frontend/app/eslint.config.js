bb9186f12c544c7bd3f367968624b96445720318
Edd <edd@eddiscoding.dev>
2025-08-20 16:42:15 +0200

    fix(frontend): :bug: fix linter for frontend project, disable pnpm-related settings

--- containers/frontend/app/eslint.config.js
+++ containers/frontend/app/eslint.config.js
@@ -2,4 +2,19 @@
 
 import { tanstackConfig } from '@tanstack/eslint-config'
 
-export default [...tanstackConfig]
+export default [
+  ...tanstackConfig,
+  {
+    ignores: ['eslint.config.js', 'prettier.config.js'],
+  },
+  {
+    rules: {
+      'tanstack/pnpm-workspace': 'off',
+      'pnpm/yaml-no-unused-catalog-item': 'off',
+      'pnpm/yaml-no-duplicate-catalog-item': 'off',
+      'pnpm/json-prefer-workspace-settings': 'off',
+      'pnpm/json-valid-catalog': 'off',
+      'pnpm/json-enforce-catalog': 'off',
+    },
+  },
+]
